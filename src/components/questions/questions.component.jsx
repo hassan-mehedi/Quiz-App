c0a645c9de2ee8aea8183fa3c4ba313bd8821566
Mehedi Hassan <howlader.mehedihassan@gmail.com>
2022-01-21 11:40:07 +0600

    feat: add initial folders and packages

---
+++ src/components/questions/questions.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./questions.style.scss";
+
+export default function Questions() {
+    return (
+        <div>
+            <h1>Questions</h1>
+        </div>
+    );
+}
